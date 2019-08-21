1325b3a62782ac1b9c3c601224a712d7ff739b95
Michael C. Frank <mcfrank@stanford.edu>
2019-08-21 16:18:58 -0700

    added accuracy plots

--- viz_app/server.R
+++ viz_app/server.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(eyetrackingR)
 library(ggthemes)
+library(langcog)
+
+ci.95 <- function(x) {
+  n <- sum(!is.na(x))
+  sem <- sd(x, na.rm = TRUE) / sqrt(n)
+  return(c(qnorm(0.025)*sem, qnorm(0.975)*sem))
+}
 
 aoi_data_sample <- read_csv(here::here("demo_data/aoi_data.csv"))
 dataset_sample <- read_csv(here::here("demo_data/dataset.csv"))
@@ -266,10 +273,100 @@ server <- function(input, output, session) {
   })
   
   output$accuracy_plot <- renderPlot({
-    ggplot()
+    acc_means <- aoi_data() %>%
+    # acc_means <- foo %>%
+      group_by(sub_id, trial_id, age_binned, target_label) %>%
+      filter(t >= input$analysis_window_range[1],
+             t <= input$analysis_window_range[2]) %>%
+      summarise(prop_looking = mean(aoi == "target", na.rm = TRUE)) %>%
+      group_by(age_binned, target_label) %>%
+      summarise(mean = mean(prop_looking, na.rm = TRUE),
+                ci_lower = mean + ci.95(prop_looking)[1],
+                ci_upper = mean + ci.95(prop_looking)[2],
+                n = n())
+             
+    if (input$age_facet) {
+      p <- ggplot(acc_means, 
+                  aes(x = age_binned, y = mean, fill = age_binned)) + 
+        geom_bar(stat="identity") +
+        facet_wrap(~target_label)
+    } else {
+      p <- ggplot(acc_means, 
+                  aes(x = target_label, y = mean, fill = target_label)) + 
+        geom_bar(stat="identity") +
+        facet_wrap(~age_binned)
+    }
+    p + 
+      geom_linerange(aes(ymin = ci_lower, ymax = ci_upper)) +
+      geom_hline(yintercept = .5, lty = 2) + 
+      ylim(0,1) + 
+      ylab("Proportion Target Looking") +
+      xlab("Age (binned)") +
+      theme_classic() +
+      scale_color_solarized() +
+      scale_fill_solarized()
+      
   })
   
   output$rt_plot <- renderPlot({
     ggplot()
   })
 }
+
+# 
+# # Define server logic required to draw RT histograms ----
+# rt_result <- d %>% 
+#   group_by(sub_id, trial_id) %>% 
+#   nest() %>% 
+#   mutate(rt = purrr::map(data, .f = compute_rt, sampling_rate = 33)) %>% 
+#   unnest(rt, .drop = T) %>% 
+#   filter(shift_type %in% c("D-T", "T-D")) %>% 
+#   left_join(d_participants, by = c("sub_id")) %>% 
+#   mutate(age_binned = cut_number(age, n = 3))
+# 
+# rt_ylim_buffer <- 100
+# 
+# server <- function(input, output) {
+#   # filter rts
+#   observeEvent( input$filter, {
+#     rt_filt <- rt_result %>% 
+#       filter(rt_value >= as.numeric(input$rt_window[1]),
+#              rt_value <= as.numeric(input$rt_window[2]),
+#              fst_shift_gap_ms <= as.numeric(input$max_fst_gap))
+#     
+#     # compute median rts for different shift types
+#     rt_median <- rt_filt %>% 
+#       group_by(shift_type) %>% 
+#       summarise(rt_median = median(rt_value))
+#     
+#     # compute mean rt for each participant based on filtering
+#     ss_rt <- rt_filt %>% 
+#       filter(shift_type == "D-T", !is.na(rt_value)) %>% 
+#       group_by(sub_id, age_binned, age) %>% 
+#       summarise(m_rt = mean(rt_value)) %>% 
+#       ungroup() %>% 
+#       mutate(min_rt = min(m_rt),
+#              max_rt = max(m_rt))
+#     
+#     # Histogram of RTs in peekbank data ----
+#     # with requested number of bins and RT filters
+#     output$plot <- renderPlot({
+#       
+#       rt_hist <- rt_filt %>% 
+#         ggplot(aes(x = rt_value)) +
+#         geom_histogram(bins = input$bins, color = "black", alpha = 0.4) +
+#         labs(x = "RT (msec)", y = "Count") +
+#         facet_wrap(~shift_type, ncol = 1) +
+#         geom_vline(data = rt_median, aes(xintercept = rt_median), 
+#                    color = "darkorange", size = 1.5)
+#       
+#       age_scatter <- ss_rt %>% 
+#         ggplot(aes(x = age, y = m_rt)) +
+#         geom_point(shape = 21, size = 3, color = "black", 
+#                    fill = "grey") +
+#         geom_smooth(method = "lm") +
+#         lims(y = c(0, ss_rt$max_rt[1] + rt_ylim_buffer)) +
+#         labs(x = "Age (months)", y = "Average RT (msec)")
+#       
+#       
+#       cowplot::plot_grid(rt_hist, age_scatter, scale = c(1, 0.9))
